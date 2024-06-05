7eac2864ffafbb5504b12f8cd2473661c441fea4
sukronsabari <sukronsabari11@gmail.com>
2024-06-06 02:05:42 +0800

    format price in thanyou page

--- app/(routes)/thank-you/ThankYou.tsx
+++ app/(routes)/thank-you/ThankYou.tsx
@@ -19,6 +19,8 @@ export function ThankYou() {
     retryDelay: 500,
   });
 
+  const SHIPPING_AMOUNT = 500;
+
   if (data === undefined) {
     return (
       <div className="min-h-screen">
@@ -140,15 +142,19 @@ export function ThankYou() {
         <div className="space-y-6 border-t border-zinc-200 pt-10 text-sm">
           <div className="flex justify-between">
             <p className="font-medium text-zinc-900">Subtotal</p>
-            <p className="text-zinc-700">{formatPrice(amount)}</p>
+            <p className="text-zinc-700">{formatPrice(amount / 100)}</p>
           </div>
           <div className="flex justify-between">
             <p className="font-medium text-zinc-900">Shipping</p>
-            <p className="text-zinc-700">{formatPrice(0)}</p>
+            <p className="text-zinc-700">
+              {formatPrice(SHIPPING_AMOUNT / 100)}
+            </p>
           </div>
           <div className="flex justify-between">
             <p className="font-medium text-zinc-900">Total</p>
-            <p className="text-zinc-700">{formatPrice(amount)}</p>
+            <p className="text-zinc-700">
+              {formatPrice(amount / 100 + SHIPPING_AMOUNT / 100)}
+            </p>
           </div>
         </div>
       </div>
